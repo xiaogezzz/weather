22276d2e979f1f1124e1bab68cee493d739c02cd
Shawn-newteng <844699335@qq.com>
2021-05-24 15:57:07 +0800

    finish code

---
+++ tests/WeatherTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Shawn\Weather\Tests;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7\Response;
+use Mockery\Matcher\AnyArgs;
+use Shawn\Weather\Exceptions\HttpException;
+use Shawn\Weather\Exceptions\InvalidArgumentException;
+use Shawn\Weather\Weather;
+use PHPUnit\Framework\TestCase;
+
+class WeatherTest extends TestCase
+{
+    public function testGetWeather()
+    {
+        // json test
+        $response = new Response(200, [], '{"success": true}');
+        $client = \Mockery::mock(Client::class);
+
+        $client->allows()->get('https://restapi.amap.com/v3/weather/weatherInfo', [
+            'query' => [
+                'key' => 'mock-key',
+                'city' => '重庆',
+                'output' => 'json',
+                'extensions' => 'base',
+            ]
+        ])->andReturn($response);
+
+        $w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+        $w->allows()->getHttpClient()->andReturn($client);
+
+        $this->assertSame(['success' => true], $w->getWeather('重庆'));
+
+        // xml test
+        $response = new Response(200, [], '<hello>content</hello>');
+        $client = \Mockery::mock(Client::class);
+        $client->allows()->get('https://restapi.amap.com/v3/weather/weatherInfo', [
+            'query' => [
+                'key' => 'mock-key',
+                'city' => '重庆',
+                'extensions' => 'all',
+                'output' => 'xml',
+            ],
+        ])->andReturn($response);
+
+        $w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+        $w->allows()->getHttpClient()->andReturn($client);
+
+        $this->assertSame('<hello>content</hello>', $w->getWeather('重庆', 'all', 'xml'));
+    }
+
+    public function testGetHttpClient()
+    {
+        $w = new Weather('mock-key');
+
+        $this->assertInstanceOf(ClientInterface::class, $w->getHttpClient());
+    }
+
+    public function testSetGuzzleOptions()
+    {
+        $w = new Weather('mock-key');
+
+        $this->assertNull($w->getHttpClient()->getConfig('timeout'));
+
+        // 设置参数
+        $w->setGuzzleOptions(['timeout' => 5000]);
+
+        // 设置参数后，timeout 为 5000
+        $this->assertSame(5000, $w->getHttpClient()->getConfig('timeout'));
+    }
+
+    public function testGetWeatherWithInvalidType()
+    {
+        $w = new Weather('mock-key');
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage('Invalid type value(base/all): foo');
+
+        $w->getWeather('重庆', 'foo');
+
+        $this->fail('Failed to assert getWeather throw exception with invalid argument.');
+    }
+
+    public function testGetWeatherWithInvalidFormat()
+    {
+        $w = new Weather('mock-key');
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage('Invalid response format: array');
+
+        $w->getWeather('重庆', 'base', 'array');
+
+        $this->fail('Failed to assert getWeather throw exception with invalid argument.');
+    }
+
+    public function testGetWeatherWithGuzzleRuntimeException()
+    {
+        $client = \Mockery::mock(Client::class);
+        $client->allows()
+            ->get(new AnyArgs())
+            ->andThrow(new \Exception('request timeout'));
+
+        $w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+        $w->allows()->getHttpClient()->andReturn($client);
+
+        $this->expectException(HttpException::class);
+        $this->expectExceptionMessage('request timeout');
+
+        $w->getWeather('重庆');
+    }
+}
